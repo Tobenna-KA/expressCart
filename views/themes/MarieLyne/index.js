d8bb7531b276cb2f6e71ff75fbf94885e4e5c563
Jeremiah Akpera <jerryakpera@gmail.com>
2020-11-09 18:05:05 +0100

    Footer

--- views/themes/MarieLyne/index.js
+++ views/themes/MarieLyne/index.js
@@ -18,17 +18,4 @@ $(document).ready(function () {
     dots: false,
     label: false,
   });
-
-  // *** CURRENCY SELECTER ***
-  $('#sm-close').click(function () {
-    $('.currency-box').addClass('sm-collapse');
-    $('#sm-open').delay(300).css('left', '0');
-    alert('SOLID');
-  });
-
-  $('#sm-open').click(function () {
-    alert('SOLID');
-    $('#sm-open').css('left', '-60px');
-    $('.currency-box').removeClass('sm-collapse');
-  });
 });
